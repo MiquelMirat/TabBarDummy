a02b1ec06ae8f8d8361d9d20b76d04bfb7b134b2
Miquel Mirat <1166miki@gmail.com>
2019-01-08 19:27:43 +0100

    Initial Commit

---
+++ TabBarDummy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarDummy
+//
+//  Created by Miquel Maria Mirat on 08/01/2019.
+//  Copyright © 2019 Miquel Maria Mirat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
